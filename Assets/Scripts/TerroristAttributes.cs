52333a9be3d2affdb4e3a11899cacca373b0e831
多喝凉水。 <43728183+Folivora0707@users.noreply.github.com>
2022-06-20 01:53:38 +0100

    hide

--- Assets/Scripts/TerroristAttributes.cs
+++ Assets/Scripts/TerroristAttributes.cs
@@ -15,6 +15,55 @@ public class TerroristAttributes : MonoBehaviour
 
     public float meleeDamage;
     public float fireRate;
-    public float shootingAccuracy; 
+    public float shootingAccuracy;
+
+    private void Update()
+    {
+        PoliceAttributes[] enemies = FindObjectsOfType<PoliceAttributes>();
+        PoliceAttributes firstEnemy = FindFirstEnemy(enemies, this.transform);
+        MeshRenderer[] allRenderers = transform.GetComponentsInChildren<MeshRenderer>();
+        if (Vector3.Distance(firstEnemy.transform.position, transform.position) > 10)
+        {
+            HideRenderers(allRenderers); 
+        }
+        else
+        {
+            ShowRenderers(allRenderers); 
+        }
+    }
+
+    void HideRenderers(MeshRenderer[] allRenderers) 
+    {
+        for (int i = 0; i < allRenderers.Length; i++)
+        {
+            allRenderers[i].enabled = false; 
+        }
+    }
+
+    void ShowRenderers(MeshRenderer[] allRenderers)
+    {
+        for (int i = 0; i < allRenderers.Length; i++)
+        {
+            allRenderers[i].enabled = true; 
+        }
+    }
+
+    PoliceAttributes FindFirstEnemy(PoliceAttributes[] enemies, Transform policePos)
+    {
+        float minDistance = Vector3.Distance(enemies[0].transform.position, policePos.position);
+        PoliceAttributes finalEnemy = enemies[0];
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            float nextDistance = Vector3.Distance(enemies[i].transform.position, policePos.position);
+            if (nextDistance < minDistance)
+            {
+                finalEnemy = enemies[i];
+                minDistance = nextDistance;
+            }
+        }
+
+        return finalEnemy;
+    }
 
 }
