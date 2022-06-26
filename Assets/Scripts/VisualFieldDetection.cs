e7390b9458ae263af89965babc3a50a5d42542df
多喝凉水。 <43728183+Folivora0707@users.noreply.github.com>
2022-06-26 22:31:54 +0100

    twokindsofline

--- Assets/Scripts/VisualFieldDetection.cs
+++ Assets/Scripts/VisualFieldDetection.cs
@@ -9,11 +9,17 @@ public class VisualFieldDetection : MonoBehaviour
     private float addHighOfAngle; 
     private Vector3 origin;
 
+    private LineRenderer lr;
+
+    public Gradient lineColor; 
+
     private void Start()
     {
         rays = new List<Ray>(); 
         fieldAngle = 30;
-        addHighOfAngle = (Mathf.Tan(((fieldAngle/2)*Mathf.PI)/180))/(fieldAngle/2);
+        addHighOfAngle = (Mathf.Tan(((fieldAngle/2)*Mathf.PI)/180))/(fieldAngle);
+
+        lr = GetComponent<LineRenderer>(); 
     }
 
     private void Update()
@@ -25,19 +31,20 @@ public class VisualFieldDetection : MonoBehaviour
     void CreateRays()
     {
         if (rays.Count != 0) rays.Clear(); 
-        origin = new Vector3(transform.position.x, transform.position.y, transform.position.z + (float)0.2); 
 
-        Vector3 direction = new Vector3(1, 0, 0).normalized;
+        origin = transform.position; 
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 direction = new Vector3(forward.x, forward.y, forward.z).normalized;   
         Ray ray = new Ray(origin, direction);
         rays.Add(ray); 
 
-        for (int i = 1; i < fieldAngle / 2; i++)
+        for (int i = 1; i < fieldAngle; i++)
         {
-            direction = new Vector3(1, 0, addHighOfAngle * i).normalized;
+            direction = new Vector3(forward.x, forward.y, forward.z + addHighOfAngle * i).normalized;
             ray = new Ray(origin, direction);
             rays.Add(ray);
 
-            direction = new Vector3(1, 0, -addHighOfAngle * i).normalized;
+            direction = new Vector3(forward.x, forward.y, forward.z - addHighOfAngle * i).normalized; 
             ray = new Ray(origin, direction);
             rays.Add(ray); 
         }
@@ -46,9 +53,19 @@ public class VisualFieldDetection : MonoBehaviour
 
     void DrawRays()
     {
+        lr.colorGradient = lineColor; 
+
+        int postionCount = 2;
         foreach (var ray in rays)
         {
-            Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);  
+            // Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+
+            lr.positionCount = postionCount;
+            lr.SetPosition(postionCount - 2, ray.origin);
+            Vector3 endPoint = ray.origin + ray.direction * 10;
+            lr.SetPosition(postionCount - 1, endPoint); 
+
+            postionCount += 2; 
         }
     }
 }
