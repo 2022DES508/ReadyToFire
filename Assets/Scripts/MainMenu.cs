c31e2f9c404523fd71d752761ba18ff719c12c4b
Harry McGuigan <harrymcguigan7@gmail.com>
2022-07-25 18:48:28 +0100

    Level Select Menu Function complete.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayArea1()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayArea2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void PlayArea3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void PlayArea4()
+    {
+        SceneManager.LoadScene(4);
     }
 
     public void QuitGame()
